d48c15a3ab4017b1616e90ee1f90bd635fd929b7
Sergey Shmakov <sergey-sti36@yandex.ru>
2024-06-05 19:00:35 +0300

    абсолютная_сумма

---
+++ python/module_3 data type/lesson_1/a, b, c, d, e = float(input()), float(in2.py
@@ -0,0 +1,6 @@
+a, b, c, d, e = float(input()), float(input()), float(input()), float(input()), float(input())
+s = abs(a) + abs(b) + abs(c) + abs(d) + abs(e)
+print(s)
+
+"""Даны пять чисел a1, a2, a3, a4, a5. Напишите программу, которая вычисляет сумму их модулей 
+∣a1∣ + ∣a2∣ + ∣a3∣ + ∣a4∣ + ∣a5∣."""
