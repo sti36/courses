9eaf6a0bd420de491b63c00a3f0ffebe5bd417ec
Sergey Shmakov <sergey-sti36@yandex.ru>
2024-06-05 18:21:13 +0300

    сортировка_трёх

---
+++ python/module_3 data type/lesson_1/a, b, c = int(input), int(input), int(in.py
@@ -0,0 +1,7 @@
+a, b, c = int(input()), int(input()), int(input())
+n = max(a, b, c)
+m = min(a, b, c)
+s = (a + b + c) - (n + m)
+print(n, s, m, sep='\n')
+
+#Напишите программу, которая упорядочивает три числа от большего к меньшему.
