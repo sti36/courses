abe14335f1eb368eb7efd48fc6aadcc16a5cffd1
Sergey Shmakov <sergey-sti36@yandex.ru>
2024-06-05 18:09:05 +0300

    наибольшее_и_наименьшее

---
+++ python/module_3 data type/lesson_1/a, b, c, d, e = float(input), float(inpu.py
@@ -0,0 +1,7 @@
+a, b, c, d, e = float(input()), float(input()), float(input()), float(input()), float(input())
+n = int(max(a, b, c, d, e))
+m = int(min(a, b, c, d, e))
+print('Наименьшее число =', m)
+print('Наибольшее число =', n)
+
+#Напишите программу, которая находит наименьшее и наибольшее из пяти чисел.
