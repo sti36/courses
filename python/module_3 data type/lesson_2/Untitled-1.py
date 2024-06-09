35d24268e4cfd053d84e2b6be2e27fdb4dd72a9e
Sergey Shmakov <sergey-sti36@yandex.ru>
2024-06-09 14:58:32 +0300

    Текст

---
+++ python/module_3 data type/lesson_2/Untitled-1.py
@@ -0,0 +1,7 @@
+print('"Python is a great language!", said Fred.', '"I don'"'t", 'ever remember having this much fun before."')
+
+"""
+Напишите программу, которая выводит текст:
+
+"Python is a great language!", said Fred. "I don't ever remember having this much fun before."
+"""
