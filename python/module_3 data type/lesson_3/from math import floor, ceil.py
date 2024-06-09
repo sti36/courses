88e3243c1e7b028bcf4e16f0cc25bf4b23e50db3
Sergey Shmakov <sergey-sti36@yandex.ru>
2024-06-09 16:58:29 +0300

    Пол_и_потолок

---
+++ python/module_3 data type/lesson_3/from math import floor, ceil.py
@@ -0,0 +1,7 @@
+from math import floor, ceil
+
+x = float(input())
+s = floor(x) + ceil(x)
+print(s)
+
+#Напишите программу, которая принимает на вход действительное число x и вычисляет по нему значение
