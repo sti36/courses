617bb5cf9a17a68aef38a0ecb139ac5cc6b69cc1
Sergey Shmakov <sergey-sti36@yandex.ru>
2024-06-09 15:04:48 +0300

    What's_Your_Name?

---
+++ python/module_3 data type/lesson_2/name = input().py
@@ -0,0 +1,9 @@
+name = input()
+family = input()
+print('Hello', name, family + '!', 'You have just delved into Python')
+
+"""
+Напишите программу, которая считывает с клавиатуры две строки – имя и фамилию пользователя и выводит фразу:
+
+Hello <введённое имя> <введённая фамилия>! You have just delved into Python
+"""
