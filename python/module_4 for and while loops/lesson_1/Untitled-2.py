54d37ee8fd9ce517233d4fe48c576f64e72a8a9f
Sergey Shmakov <sergey-sti36@yandex.ru>
2024-06-09 18:12:28 +0300

    Последовательность_символов

---
+++ python/module_4 for and while loops/lesson_1/Untitled-2.py
@@ -0,0 +1,13 @@
+for i in range(6):
+    print('AAA')
+for i in range(5):
+    print('BBBB')
+print('E')
+for i in range(9):
+    print('TTTTT')
+print('G')
+
+"""
+Напишите программу, которая использует ровно три цикла for для печати следующей 
+последовательности символов
+"""
