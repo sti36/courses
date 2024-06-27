1be89a6ad7f28776e6c6a247202ba33b559fed54
Sergey Shmakov <sergey-sti36@yandex.ru>
2024-06-27 19:36:12 +0300

    add_task

---
+++ python/module_5 string data type/lesson_6/year = 2010(1).py
@@ -0,0 +1,6 @@
+year = 2010
+price = '10K'
+currency = 'Bitcoin'
+s = f'In {year}, someone paid {price} {currency} for two pizzas.'
+
+print(s)
---
+++ python/module_5 string data type/lesson_6/year = 2010.py
@@ -0,0 +1,7 @@
+year = 2010
+price = '10k'
+currency = 'Bitcoin'
+
+s = 'In {0}, someone paid {1} {2} for two pizzas.'.format(year, price, currency)
+
+print(s)
