54c0c97fba84c9177eb40e54b054322747e58758
Sergey Shmakov <sergey-sti36@yandex.ru>
2024-07-02 16:39:03 +0300

    add_task

--- python/module_6 lists/lesson_1/n = int(input())1.py
+++ python/module_6 lists/lesson_1/n = int(input())1.py
@@ -1,12 +1,12 @@
+ONE_LOWERS_LETTERS_ASCII = 97
+
 n = int(input())
-one_lowers_letters_ascii = 97
 s = ''
 
 for i in range(n):
-    s += chr(one_lowers_letters_ascii + i)
+    s += chr(ONE_LOWERS_LETTERS_ASCII + i)
     
 print(list(s))
-
 '''
 Второй вариант решения с выводом элементов списка по отдельности
 list_letters_ascii = 122
---
+++ python/module_6 lists/lesson_2/languages = ['Chinese', 'Spanish', 'Engl.py
@@ -0,0 +1,6 @@
+languages = ['Chinese', 'Spanish', 'English', 'Hindi', 'Arabic', 'Bengali', 'Portuguese', 'Russian', 'Japanese', 'Lahnda']
+print(languages[::-1])
+
+"""
+Дополните приведенный код так, чтобы он вывел "перевёрнутый" список languages (т.е. элементы будут идти в обратном порядке).
+"""
